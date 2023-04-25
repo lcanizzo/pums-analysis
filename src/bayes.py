6de028f542a22adae656ffc3cee998d6d95a6c82
canil <luca.canizzo@gmail.com>
2023-04-24 20:37:34 -0400

    Naive Bayes WIP.

--- src/bayes.py
+++ src/bayes.py
@@ -3,17 +3,30 @@ Tests naive Bayes classification on data.
 """
 #%%
 import pandas as pd
-from sklearn.naive_bayes import GaussianNB
-from classification_utils import get_data, print_accuracy, print_confusion_matrix
+from sklearn.naive_bayes import CategoricalNB, GaussianNB
+from classification_utils import get_data_encoded, print_accuracy, \
+    print_confusion_matrix
 
-x_train, x_test_enc, y_train, y_test_enc = get_data()
+x_train, x_test, y_train, y_test = get_data_encoded()
 
+# Gaussian Naive Bayes
 gnb = GaussianNB()
-y_pred = gnb.fit(x_train, y_train).predict(x_test_enc)
+y_pred = gnb.fit(x_train, y_train).predict(x_test)
 
-print('\nGaussian Naive Bayes (GNB) predictions:')
+print('\nGaussian Naive Bayes predictions:')
 print(y_pred)
 
-print_accuracy(y_pred, y_test_enc)
-print_confusion_matrix(y_pred, y_test_enc)
+print_accuracy(y_pred, y_test)
+print_confusion_matrix(y_pred, y_test)
+
+# Categorical Naive Bayes
+clf = CategoricalNB(min_categories=10)
+clf.fit(x_train, y_train)
+y_pred = clf.predict(x_test)
+
+print('\nCategorical Naive Bayes predictions:')
+print(y_pred)
+
+print_accuracy(y_pred, y_test)
+print_confusion_matrix(y_pred, y_test)
 #%%
